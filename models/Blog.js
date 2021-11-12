b0d639c401166de39cb93067ad6c2d921f202be2
A'Darius Ellis <85079903+gadaell@users.noreply.github.com>
2021-11-12 15:51:01 -0500

    setup models

---
+++ models/Blog.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+// Create Blog Model
+
+class Blog extends Model {}
+
+// create fields for the Blog model
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "blog",
+  }
+);
+
+module.exports = Blog;
